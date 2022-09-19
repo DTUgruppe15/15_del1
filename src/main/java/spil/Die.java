ff20fe7da39d527449a6c7a7180e60dd9ee202c7
Magnus Ottosen <magnus@ottosen.dk>
2022-09-19 14:19:04 +0200

    Made Player and die class
    Moved all die from spil class to die class

---
+++ src/main/java/spil/Die.java
@@ -0,0 +1,21 @@
+package spil;
+
+import java.util.Random;
+
+public class Die {
+    int die;
+
+    public void setDie(int die) {
+        this.die = die;
+    }
+
+    public int getDie() {
+        return die;
+    }
+
+    public void rollDie(){
+        Random rand = new Random();
+        this.die = rand.nextInt(6)+1;
+    }
+
+}
--- src/main/java/spil/Player.java
+++ src/main/java/spil/Player.java
@@ -4,11 +4,31 @@ public class Player {
 
     private  int points;
 
+    Die die1 = new Die();
+    Die die2 = new Die();
+
     public void setPoints(int points) {
         this.points = points;
     }
 
     public int getPoints() {
         return points;
+    }
+
+    public void rollDice(){
+        die1.rollDie();
+        die2.rollDie();
+    }
+
+    public int getSum(){
+        return die1.getDie() + die2.getDie();
+    }
+
+    public boolean getIsSame(){
+        if(die1 == die2){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
