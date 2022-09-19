1781556cac33154264bb08c040864056202b93a8
Magnus Ottosen <magnus@ottosen.dk>
2022-09-19 13:41:40 +0200

    Create branch with player class

---
+++ src/main/java/spil/Player.java
@@ -0,0 +1,14 @@
+package spil;
+
+public class Player {
+
+    private  int points;
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+}
