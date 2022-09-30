5094fb6185a9f9e5a3c14068326574b64da6ff92
Magnus Ottosen <magnus@ottosen.dk>
2022-09-30 10:06:13 +0200

    DiceGame1.0
    reverted to base game to follow assignment

--- src/main/java/spil/Player.java
+++ src/main/java/spil/Player.java
@@ -31,16 +31,6 @@ public class Player {
         return die1.getDie() == die2.getDie();
     }
 
-    //This checks if the two dice are both six
-    public boolean getIsTwoSix() {
-        return die1.getDie() == 6 && die2.getDie() == 6;
-    }
-
-    //This checks if the two dice are both ones
-    public boolean getIsTwoOne() {
-        return die1.getDie() == 1 && die2.getDie() == 1;
-    }
-
     public void addPoints(int points){
         this.points += points;
     }
